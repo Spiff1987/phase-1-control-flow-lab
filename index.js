8d91abff4e3d594d8de0ffea18b6eb3d6903d6dc
dismas <afubwa@gmail.com>
2023-01-12 01:53:45 +0400

    Control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(Number){
   // Write your code here!
+  if(Number <= 400){
+    return "This one is on me!";
+  }
+  else if(Number > 400 && Number < 2000){
+    return "That will be twenty bucks."
+  }
+  else if(Number > 2000 && Number < 2500){
+    return "I will gladly take your thirty bucks.";
+  }
+  else if(Number > 2500)
+    return "No can do."
+
+  }
+  
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  if (city === "NYC"){
+    return "Ok, sounds good.";
+  }
+  else {
+    return "No go.";
+  }
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(tip){
+    case "generous":
+      return 'Thank you so much.'
+    case "not as generous":
+      return 'Thank you.'
+    default: "Thanks for everything"
+      return 'Bye.'
+  }
 }
